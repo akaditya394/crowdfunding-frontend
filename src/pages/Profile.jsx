b1c02813e1f2321834bff47c7918da1532ca2893
Aditya <akaditya394@gmail.com>
2023-01-01 22:33:26 +0530

    project completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getUserCampaigns } = useStateContext();
+
+  const fetchCampigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampigns();
+  }, [address, contract]);
+  return (
+    <div>
+      <DisplayCampaigns
+        title="Your Campaigns"
+        isLoading={isLoading}
+        campaigns={campaigns}
+      />
+    </div>
+  );
 };
 
 export default Profile;
