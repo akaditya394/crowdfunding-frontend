a1efafa7e2314c15bd337260781d815a4e03a54d
Aditya <akaditya394@gmail.com>
2023-01-01 22:47:27 +0530

    code fixed

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -47,7 +47,7 @@ const CreateCampaign = () => {
       className="bg-[#1c1c24] flex justify-center items-center flex-col
   rounded-[10px] sm:p-10 p-4"
     >
-      {isLoading && <Loading />}
+      {isLoading && <Loader />}
       <div
         className="flex justify-center items-center p-[16px] sm:min-w-[380px] 
       bg-[#3a3a43] rounded-[10px]"
